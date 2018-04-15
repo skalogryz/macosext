c3fe3bca7edda5a2176e65ad009d3f66e429c193
Dmitry Boyarintsev <skalogryz.lists@gmail.com>
2018-04-15 02:54:47 -0400

    revert previous wrong commit

--- samples/appearance/unit1.pas
+++ samples/appearance/unit1.pas
@@ -1,12 +1,10 @@
 unit Unit1;
 
 {$mode objfpc}{$H+}
-{$modeswitch objectivec1}
 
 interface
 
 uses
-  CocoaAll,
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
   macOSFormProp;
 
@@ -23,7 +21,6 @@ type
     macOSFormProp1: TmacOSFormProp;
     Memo1: TMemo;
     ScrollBar1: TScrollBar;
-    procedure Button2Click(Sender: TObject);
     procedure CheckBox1Change(Sender: TObject);
   private
 
@@ -45,26 +42,6 @@ begin
   if macOSFormProp1.Appearance <> macOSNSAppearanceNameVibrantDark
     then macOSFormProp1.Appearance := macOSNSAppearanceNameVibrantDark
     else macOSFormProp1.Appearance := macOSNSAppearanceNameAqua;
-end;
-
-procedure TForm1.Button2Click(Sender: TObject);
-var
-  bar : NSToolbar;
-  sub : NSToolbarItem;
-begin
-  bar := NSToolbar(NSToolbar(NSToolbar.alloc).initWithIdentifier(NSSTR('tool')));
-
-  sub := NSToolbarItem(NSToolbarItem.alloc).initWithItemIdentifier(NSSTR('id1'));
-  sub.autorelease;
-
-  sub.setLabel(NSSTR('hello world'));
-  sub.setPaletteLabel(NSSTR('pal hello world');
-
-
-
-  //bar.setShowsBaselineSeparator(false);
-  NSView(Self.Handle).window.setToolbar(bar);
-  //bar.insertItemWithItemIdentifier_atIndex
 end;
 
 end.
